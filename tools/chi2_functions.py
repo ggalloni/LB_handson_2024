d2261a6f62d801b9faf32d1dbf943005ddd0c57a
Giacomo Galloni <giacomo.galloni@unife.it>
2024-06-19 18:47:49 +0200

    Initial commit

---
+++ tools/chi2_functions.py
@@ -0,0 +1,86 @@
+import numpy as np
+
+
+def get_fiducial_gaussian_chi2(
+    data, template, noise, inv_cov, *, marginalize_cov=True
+):
+    Nsim = data.shape[0]
+    if marginalize_cov:
+        return -2 * np.log(
+            (
+                1
+                + np.diag((data - (template + noise)[None, :]) @ inv_cov @ (data - (template + noise)[None, :]).T)
+                / (Nsim - 1)
+            )
+            ** (-Nsim / 2)
+        )
+    return np.diag((data - (template + noise)[None, :]) @ inv_cov @ (data - (template + noise)[None, :]).T)
+
+
+def get_ideal_ptep_chi2(data, template, noise, fsky, ell, dl=None):
+    M = (data) / (template + noise)[None, :]
+
+    X = M - np.log(M) - 1
+    np.nan_to_num(X, copy=False, nan=0, posinf=0, neginf=0)
+
+    if dl is not None:
+        return np.sum((2 * ell[None, :] + 1) * fsky * X * dl)
+    return np.sum((2 * ell[None, :] + 1) * fsky * X, axis=1)
+
+
+def get_hl_chi2(
+    data,
+    template,
+    fiducial,
+    noise,
+    inv_cov,
+    *,
+    marginalize_cov=True
+):
+    Nsim = data.shape[0]
+    M = (data) / (template + noise)[None, :]
+
+    ghl = np.sign(M - 1) * np.sqrt(2 * (M - np.log(M) - 1))
+
+    X = (fiducial + noise)[None, :] * ghl
+    np.nan_to_num(X, copy=False, nan=0, posinf=0, neginf=0)
+
+    if marginalize_cov:
+        return -2 * np.log((1 + np.diag(X @ inv_cov @ X.T) / (Nsim - 1)) ** (-Nsim / 2))
+    return np.diag(X @ inv_cov @ X.T)
+
+
+def get_offset_hl_chi2(
+    data, template, fiducial, noise, offset, inv_cov, *, marginalize_cov=True
+):
+    Nsim = data.shape[0]
+    M = (data + offset[None, :]) / (template + noise + offset)[None, :]
+
+    ghl = np.sign(M - 1) * np.sqrt(2 * (M - np.log(np.abs(M)) - 1))
+
+    X = (fiducial + noise + offset)[None, :] * ghl
+    np.nan_to_num(X, copy=False, nan=0, posinf=0, neginf=0)
+
+    if marginalize_cov:
+        return -2 * np.log((1 + np.diag(X @ inv_cov @ X.T) / (Nsim - 1)) ** (-Nsim / 2))
+    return np.diag(X @ inv_cov @ X.T)
+
+
+def get_lollipop_chi2(
+    data, template, fiducial, noise, offset, inv_cov, *, marginalize_cov=True
+):
+    Nsim = data.shape[0]
+    M = (data + offset[None, :]) / (template + noise + offset)[None, :]
+
+    glolli = (
+        np.sign(M)
+        * np.sign(np.abs(M) - 1)
+        * np.sqrt(2 * (np.abs(M) - np.log(np.abs(M)) - 1))
+    )
+
+    X = (fiducial + noise + offset)[None, :] * glolli
+    np.nan_to_num(X, copy=False, nan=0, posinf=0, neginf=0)
+
+    if marginalize_cov:
+        return -2 * np.log((1 + np.diag(X @ inv_cov @ X.T) / (Nsim - 1)) ** (-Nsim / 2))
+    return np.diag(X @ inv_cov @ X.T)
---
+++ tools/common_functions.py
@@ -0,0 +1,32 @@
+import camb
+import numpy as np
+
+
+def compute_offsets(ell, varcl, clref, fsky=1.0, iter=1):
+    Nl = np.sqrt(np.abs(varcl - (2.0 / (2.0 * ell + 1) * clref**2) / fsky))
+    for _ in range(iter):
+        Nl = np.sqrt(
+            np.abs(
+                varcl - 2.0 / (2.0 * ell + 1) / fsky * (clref**2 + 2.0 * Nl * clref)
+            )
+        )
+    return Nl * np.sqrt((2.0 * ell + 1) / 2.0)
+
+
+def compute_theoretical_spectrum(lmax, r):
+    pars = camb.CAMBparams()
+    pars.set_cosmology(H0=67.32, ombh2=0.02237, omch2=0.1201, mnu=0.06, omk=0, tau=0.06)
+    pars.InitPower.set_params(As=2.12e-9, ns=0.9651, r=r)
+    pars.set_for_lmax(lmax=2500)
+    # NOTE: these parameters are the one used in the templates
+
+    pars.WantTensors = True
+    pars.DoLensing = True
+
+    results = camb.get_results(pars)
+    res = results.get_cmb_power_spectra(
+        CMB_unit="muK",
+        lmax=lmax,
+        raw_cl=True,
+    )
+    return res["total"][:, [0, 1, 2, 3]]
